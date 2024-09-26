8e4357758e903da67f73321fd1f35ead0b336d07
ataullahmesbah <mesbahataullah4@gmail.com>
2024-09-26 18:34:48 +0600

    contact number updated

--- src/app/(with-layout)/contact/page.js
+++ src/app/(with-layout)/contact/page.js
@@ -3,7 +3,7 @@ import ContactForm from '@/app/components/ContactForm/ContactForm';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { FaFacebook, FaTwitter, FaLinkedin, FaMedium, FaEnvelope, FaPhone, FaStopCircle } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaLinkedin, FaMedium, FaEnvelope, FaPhone, FaStopCircle, FaStumbleuponCircle, FaPray, FaMagic, FaFontAwesomeLogoFull, FaEdge } from 'react-icons/fa';
 
 
 export const metadata = {
@@ -65,19 +65,19 @@ const page = () => {
                     <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-8 lg:space-y-0 lg:space-x-12">
                         {/* Email */}
                         <div className="flex flex-col items-center text-center p-4 bg-gray-100 rounded-lg shadow-md border-t-8  w-full lg:w-1/3 border-2 border-t-indigo-800">
-                            <FaEnvelope className="text-4xl text-blue-700" />
+                            <FaEnvelope className="text-4xl text-gray-600" />
                             <h4 className="text-xl font-semibold mt-4">Email</h4>
                             <p className="mt-2 text-gray-700">info@ataullahmesbah.com</p>
                         </div>
                         {/* Phone */}
                         <div className="flex flex-col items-center text-center p-4 bg-gray-100 rounded-lg shadow-md border-t-8  w-full lg:w-1/3 border-2 border-t-indigo-800">
-                            <FaPhone className="text-4xl text-blue-700" />
+                            <FaPhone className="text-4xl text-gray-600" />
                             <h4 className="text-xl font-semibold mt-4">Phone</h4>
-                            <p className="mt-2 text-gray-700">+1 (555) 123-4567</p>
+                            <p className="mt-2 text-gray-700">+8809638844036</p>
                         </div>
                         {/* Social Media */}
                         <div className="flex flex-col items-center text-center p-4 bg-gray-100 rounded-lg shadow-md border-t-8  w-full lg:w-1/3 border-2 border-t-indigo-800">
-                        <FaStopCircle className="text-4xl text-blue-700" />
+                        <FaEdge className="text-4xl text-gray-600" />
                             <h4 className="text-xl font-semibold">Follow Me</h4>
                             <div className="flex space-x-6 mt-4">
                                 <a href='https://www.facebook.com/ataullah.mesbah.7' target="_blank" rel="noopener noreferrer">
